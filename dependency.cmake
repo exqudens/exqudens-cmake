da0a40e0aa35f4e95378683345e18e04d2998b50
exqudens <exqudens@gmail.com>
2020-12-27 04:12:53 -0800

    step 13

--- dependency.cmake
+++ dependency.cmake
@@ -1,7 +1,13 @@
 # set variables
-set(
-    DEPENDENCY_DEFAULT_CONFIGURE_COMMAND_TEMPLATE
-    "{{{CMAKE_COMMAND}}} -DCMAKE_INSTALL_PREFIX={{{PACKAGES_PREFIX}}}/{{{PACKAGE_NAME}}}-{{{PACKAGE_VERSION}}} -G \"{{{G_ARG}}}\" -S {{{FETCHCONTENT_BASE_DIR}}}/{{{PACKAGE_NAME}}}-{{{PACKAGE_VERSION}}}/src -B {{{FETCHCONTENT_BASE_DIR}}}/{{{PACKAGE_NAME}}}-{{{PACKAGE_VERSION}}}/build"
+set(DEPENDENCY_DEFAULT_CONFIGURE_COMMAND ${CMAKE_COMMAND})
+set(DEPENDENCY_DEFAULT_CONFIGURE_COMMAND_ARGS
+    "-DCMAKE_INSTALL_PREFIX={{{PACKAGES_PREFIX}}}/{{{PACKAGE_NAME}}}-{{{PACKAGE_VERSION}}}"
+    "-G"
+    "{{{GENERATOR}}}"
+    "-S"
+    "{{{FETCHCONTENT_BASE_DIR}}}/{{{PACKAGE_NAME}}}-{{{PACKAGE_VERSION}}}/src"
+    "-B"
+    "{{{FETCHCONTENT_BASE_DIR}}}/{{{PACKAGE_NAME}}}-{{{PACKAGE_VERSION}}}/build"
 )
 
 # enable extensions
@@ -33,6 +39,27 @@ function(dependency)
     set(multiValueArgs)
     cmake_parse_arguments(PARSE_ARGV 0 DEPENDENCY "${options}" "${oneValueArgs}" "${multiValueArgs}")
 
+    # set 'DEPENDENCY_URL'
+    if(EXISTS "${DEPENDENCY_DOWNLOADS_PREFIX}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}/${DEPENDENCY_DOWNLOAD_NAME}")
+        set(DEPENDENCY_URL "file://${DEPENDENCY_DOWNLOADS_PREFIX}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}/${DEPENDENCY_DOWNLOAD_NAME}")
+    else()
+        set(DEPENDENCY_URL "${DEPENDENCY_URL_SCHEMA}://${DEPENDENCY_URL_HOST}")
+    endif()
+
+    # set 'DEPENDENCY_CONFIGURE_COMMAND'
+    set(DEPENDENCY_CONFIGURE_COMMAND "${DEPENDENCY_DEFAULT_CONFIGURE_COMMAND}")
+
+    # set 'DEPENDENCY_CONFIGURE_COMMAND_ARGS'
+    set(DEPENDENCY_CONFIGURE_COMMAND_ARGS)
+    foreach(arg ${DEPENDENCY_DEFAULT_CONFIGURE_COMMAND_ARGS})
+        string(REPLACE "{{{GENERATOR}}}"             "${CMAKE_EXTRA_GENERATOR} - ${CMAKE_GENERATOR}" arg "${arg}")
+        string(REPLACE "{{{PACKAGES_PREFIX}}}"       "${DEPENDENCY_PACKAGES_PREFIX}"                 arg "${arg}")
+        string(REPLACE "{{{PACKAGE_NAME}}}"          "${DEPENDENCY_PACKAGE_NAME}"                    arg "${arg}")
+        string(REPLACE "{{{PACKAGE_VERSION}}}"       "${DEPENDENCY_PACKAGE_VERSION}"                 arg "${arg}")
+        string(REPLACE "{{{FETCHCONTENT_BASE_DIR}}}" "${FETCHCONTENT_BASE_DIR}"                      arg "${arg}")
+        list(APPEND DEPENDENCY_CONFIGURE_COMMAND_ARGS "${arg}")
+    endforeach()
+
     message(STATUS "---")
 
     message(STATUS "DEPENDENCY_NAME:             '${DEPENDENCY_NAME}'")
@@ -52,16 +79,13 @@ function(dependency)
 
     message(STATUS "---")
 
-    set(INTERNAL_VAR_1 "${DEPENDENCY_DEFAULT_CONFIGURE_COMMAND_TEMPLATE}")
+    message(STATUS "DEPENDENCY_URL: '${DEPENDENCY_URL}'")
+    message(STATUS "DEPENDENCY_CONFIGURE_COMMAND: ${DEPENDENCY_CONFIGURE_COMMAND}")
+    message(STATUS "DEPENDENCY_CONFIGURE_COMMAND_ARGS: ${DEPENDENCY_CONFIGURE_COMMAND_ARGS}")
 
-    string(REPLACE "{{{CMAKE_COMMAND}}}" "${CMAKE_COMMAND}" INTERNAL_VAR_1 "${INTERNAL_VAR_1}")
-    string(REPLACE "{{{PACKAGES_PREFIX}}}" "${CMAKE_BINARY_DIR}/packages" INTERNAL_VAR_1 "${INTERNAL_VAR_1}")
-    string(REPLACE "{{{PACKAGE_NAME}}}" "zlib" INTERNAL_VAR_1 "${INTERNAL_VAR_1}")
-    string(REPLACE "{{{PACKAGE_VERSION}}}" "1.2.11" INTERNAL_VAR_1 "${INTERNAL_VAR_1}")
-    string(REPLACE "{{{G_ARG}}}" "CodeBlocks - MinGW Makefiles" INTERNAL_VAR_1 "${INTERNAL_VAR_1}")
-    string(REPLACE "{{{FETCHCONTENT_BASE_DIR}}}" "${FETCHCONTENT_BASE_DIR}" INTERNAL_VAR_1 "${INTERNAL_VAR_1}")
+    message(STATUS "---")
 
-    set(TEST_VAR_1 "${INTERNAL_VAR_1}" PARENT_SCOPE)
+    #set(TEST_VAR_1 "AAAbbbCCCddd" PARENT_SCOPE)
 
     # process -----------------------------------------------------------------------
 
@@ -74,10 +98,15 @@ function(dependency)
 
     # build if not found '${DEPENDENCY_NAME}'
     if(NOT ${${DEPENDENCY_NAME}_FOUND})
+
+        message(STATUS "--- extract '${DEPENDENCY_PACKAGE_NAME}' (start) ---")
         FetchContent_Declare("fetch_content_${DEPENDENCY_PACKAGE_NAME}"
             DOWNLOAD_DIR  "${DEPENDENCY_DOWNLOADS_PREFIX}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}"
             DOWNLOAD_NAME "${DEPENDENCY_DOWNLOAD_NAME}"
-            URL           "${DEPENDENCY_URL_SCHEMA}://${DEPENDENCY_URL_HOST}"
+            SOURCE_DIR    "${FETCHCONTENT_BASE_DIR}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}/src"
+            BINARY_DIR    "${FETCHCONTENT_BASE_DIR}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}/build"
+            SUBBUILD_DIR  "${FETCHCONTENT_BASE_DIR}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}/subbuild"
+            URL           "${DEPENDENCY_URL}"
             URL_MD5       "${DEPENDENCY_URL_MD5}"
             #CMAKE_ARGS    "-DCMAKE_INSTALL_PREFIX=${DEPENDENCY_PACKAGES_PREFIX}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}"
         )
@@ -85,6 +114,25 @@ function(dependency)
         if(NOT ${fetch_content_${DEPENDENCY_PACKAGE_NAME}_POPULATED})
             FetchContent_Populate("fetch_content_${DEPENDENCY_PACKAGE_NAME}")
         endif()
+        if(NOT ${fetch_content_${DEPENDENCY_PACKAGE_NAME}_POPULATED})
+            message(FATAL_ERROR "ERROR: 'fetch_content_${DEPENDENCY_PACKAGE_NAME}' not populated !!!")
+        endif()
+        message(STATUS "--- extract '${DEPENDENCY_PACKAGE_NAME}' (done) ---")
+
+        message(STATUS "--- config '${DEPENDENCY_PACKAGE_NAME}' (start) ---")
+        execute_process(
+            COMMAND           ${DEPENDENCY_CONFIGURE_COMMAND} ${DEPENDENCY_CONFIGURE_COMMAND_ARGS}
+            WORKING_DIRECTORY "${FETCHCONTENT_BASE_DIR}/${DEPENDENCY_PACKAGE_NAME}-${DEPENDENCY_PACKAGE_VERSION}"
+            RESULT_VARIABLE   "fetch_content_${DEPENDENCY_PACKAGE_NAME}_CONFIG_RESULT"
+        )
+        if(NOT ${fetch_content_${DEPENDENCY_PACKAGE_NAME}_CONFIG_RESULT} EQUAL 0)
+            message(
+                FATAL_ERROR
+                "ERROR: config 'fetch_content_${DEPENDENCY_PACKAGE_NAME}' exit code ${fetch_content_${DEPENDENCY_PACKAGE_NAME}_CONFIG_RESULT} !!!"
+            )
+        endif()
+        message(STATUS "--- config '${DEPENDENCY_PACKAGE_NAME}' (done) ---")
+
     endif()
 
 endfunction()
