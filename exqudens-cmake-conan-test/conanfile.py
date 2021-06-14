32b9ef27948e4015c067b8a179f7901e8334a6e0
exqudens <exqudens@gmail.com>
2021-06-13 21:29:46 -0700

    step 52

--- exqudens-cmake-conan-test/conanfile.py
+++ exqudens-cmake-conan-test/conanfile.py
@@ -5,10 +5,12 @@ class ConanConfiguration(ConanFile):
     requires = [
         "zlib/1.2.11",
         "libpng/1.6.37",
-        "yaml-cpp/0.6.3"
+        "json-c/0.15"
     ]
     settings = "arch", "os", "compiler", "build_type"
     options = {"shared": [True, False]}
+    generators = "cmake_paths", "cmake_find_package"
 
-    def imports(self):
-        self.copy(pattern="*", folder=True)
+
+if __name__ == "__main__":
+    pass
