2234ae86b3b3bf26155693bcda7daeb72e4b1015
exqudens <exqudens@gmail.com>
2021-09-19 03:33:59 -0700

    step 66

--- cmake-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cmake -S "." --preset "${1}"
-cmake --build --preset "${1}" --target "exqudens-cmake-cpp-test-executable"
-echo 'BUILD_SUCCESSFUL' || (echo 'BUILD_FAILED' && false)
--- cmake-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cmake -S "." --preset "${1}"
-echo 'BUILD_SUCCESSFUL' || (echo 'BUILD_FAILED' && false)
